fe752bd3c4caa82a738d74be05bc8c259d507134
Peter Yoon <peyo.hd@gmail.com>
2019-08-10 15:59:11 +0900

    MainActivity : add onSendClicked()
      - http access to server

--- src/com/peyo/loadcheck/MainActivity.java
+++ src/com/peyo/loadcheck/MainActivity.java
@@ -12,6 +12,14 @@ import android.os.HardwarePropertiesManager;
 import android.util.Log;
 import android.view.View;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class MainActivity extends Activity {
     private static final String TAG = "MainActivity";
 
@@ -69,5 +77,36 @@ public class MainActivity extends Activity {
         ActivityManager am = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
         am.getMemoryInfo(mi);
         Log.i(TAG, "MemoryUsage : " + (float)(mi.totalMem - mi.availMem)/mi.totalMem);
+    }
+
+    public void onSendClicked(View v) {
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... voids) {
+                String url = "http://<ip-addr>/api/log/RemoteControl?deviceid=<deviceid>";
+                Log.i(TAG, "onSendClicked() URL :" + url);
+                try {
+                    HttpURLConnection conn = (HttpURLConnection) (new URL(url).openConnection());
+                    conn.setConnectTimeout(1000);
+                    int resp = conn.getResponseCode();
+                    Log.i(TAG, "Response code : " + resp);
+
+                    if (resp == HttpURLConnection.HTTP_OK) {
+                        BufferedReader in = new BufferedReader(new InputStreamReader(
+                                conn.getInputStream()));
+                        String content = in.readLine();
+                        Log.i(TAG, "Content : " + content);
+
+                        JSONObject json = new JSONObject(content);
+                        Log.i(TAG, "actionid : " + json.getString("actionid"));
+                    }
+
+                    conn.disconnect();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+        }.execute();
     }
 }
